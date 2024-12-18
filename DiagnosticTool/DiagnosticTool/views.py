a49688497560ac3c90f5f25c14a3b4bd640c6c77
doha <doha.aberkane@yahoo.com>
2024-12-18 11:01:13 +0100

    ajout files for page accueil

--- DiagnosticTool/DiagnosticTool/views.py
+++ DiagnosticTool/DiagnosticTool/views.py
@@ -1,12 +1,2 @@
-"""
-TO DO (usecase) :
-
-*possibility to check the main page only if user connected (in html --> use {%%})
-*get info form (create in form : "ScenarioForm" )to have the possibility to generate a scenario
-    --> possibility to launch a diagnostic
-        --> button that will redirect to result page analysis
-            --> page analysis == creation of another html page linked to this app
-                --> show the description scenario and next to it the analysis
-                --> possibility to export scenario in pdf format
-*partie historique --> get the history of the files generated bfr  --> table in model to get the file names (to check if we need to download it) depending on which user is connected
-"""
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
