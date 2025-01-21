7b0f3adb285e06d4075633cb0ac3b5b8f25dbe58
andinox <ncls.rdl@outlook.fr>
2025-01-21 14:34:28 +0100

    add login page with django contrib

--- DiagnosticTool/DiagnosticTool/urls.py
+++ DiagnosticTool/DiagnosticTool/urls.py
@@ -9,19 +9,16 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')r
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from app import views
+from django.urls import path, include
+from GestionnaireUsers import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', views.home, name='home'),
-    path('', views.home, name='home'),
-
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
