0d3ba891df09c1f87482c0c45c172b66f5ff8c52
claire <claire.mrtn13@gmail.com>
2025-01-27 21:28:48 +0100

    Récupérer infos des interfaces / les afficher

---
+++ Tests/analyse_interfaces.py
@@ -0,0 +1,32 @@
+import json
+
+file_path = "/var/log/telegraf/interfaces.out"
+
+
+# Lecture du fichier json contenant les informations sur les statuts des interfaces
+def read_metrics(file_path):
+    with open(file_path, "r") as f:
+        for line in f:
+            data = json.loads(line.strip())
+            affichage_json = json.dumps(data, indent=2)
+            print(affichage_json)
+
+#Avec filtre des champs qui nous intressenté
+def filtre_status(file_path):
+    with open(file_path, "r") as f:
+        for line in f:
+            data = json.loads(line.strip())
+            # Extraction information
+            interface_name = data["tags"]["name"]
+            admin_status = data["fields"].get("admin_status")
+            op_status = data["fields"].get("oper_status")
+
+            # Affichage
+            print("Interface : ", interface_name)
+            print("Admin Status : ", admin_status)
+            print("Operational Status : ", op_status)
+            print("----------------------------------------")
+
+
+if __name__ == "__main__":
+    filtre_status(file_path)
