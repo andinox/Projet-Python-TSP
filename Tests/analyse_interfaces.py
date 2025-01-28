1af634f66241b4cfa8ad9b09c85384638954507c
Claire M <84991362+Eclaizr@users.noreply.github.com>
2025-01-28 13:45:49 +0100

    Update analyse_interfaces.py

--- Tests/analyse_interfaces.py
+++ Tests/analyse_interfaces.py
@@ -19,7 +19,7 @@ def filtre_status(file_path):
             # Extraction information
             interface_name = data["tags"]["name"]
             admin_status = data["fields"].get("admin_status")
-            op_status = data["fields"].get("oper_status")
+            op_status = data["fields"].get("op_status")
 
             # Affichage
             print("Interface : ", interface_name)
